0cd18076969139c3a5b0a230cece9292b8067e3a
Melbyn <mxutucb@miumg.edu.gt>
2025-07-30 23:21:45 -0600

    Creación de respositorio del tipo de cafe.

---
+++ src/main/java/com/umg/cafe/repositories/TipoCafeRepository.java
@@ -0,0 +1,8 @@
+package com.umg.cafe.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.umg.cafe.entities.TipoCafe;
+
+public interface TipoCafeRepository extends JpaRepository<TipoCafe, Long> {}
+
